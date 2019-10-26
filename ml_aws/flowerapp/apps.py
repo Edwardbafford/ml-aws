f1e8d466bddb3d4fa6c0e0574b85be67bd654c24
Louie Bafford <Louie.Bafford@bracketglobal.com>
2019-10-26 13:04:24 -0400

    delete old monolithic app directory

--- ml_aws/flowerapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FlowerappConfig(AppConfig):
-    name = 'flowerapp'
--- ml_aws/flowerapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index_test, name='index_test'),
-    path('upload', views.flower, name='flower')
-]
