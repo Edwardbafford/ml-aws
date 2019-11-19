ce0729d36134fe52d9eb320be3c09d1a2f451a07
Louie Bafford <Louie.Bafford@bracketglobal.com>
2019-11-19 17:13:24 -0500

    Front End updates

--- Services/controller/controller/settings.py
+++ Services/controller/controller/settings.py
@@ -117,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'),
+)
